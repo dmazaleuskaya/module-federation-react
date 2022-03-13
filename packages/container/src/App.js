f85812d396915787609c2f844c596a9ea2bb63a0
Darya Mazaleuskaya <dariayarohovich@gmail.com>
2022-03-13 13:06:22 +0100

    Noop change to trigger workflow

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { MarketingApp } from "./components/marketing-app";
 
-export const App = () => {
-  return (
-    <div>
-      <h1>Hi there in Container</h1>
-      <hr />
-      <MarketingApp />
-    </div>
-  );
-};
+export const App = () => (
+  <div>
+    <h1>Hi there in Container</h1>
+    <hr />
+    <MarketingApp />
+  </div>
+);
