4de1d4ff02a35bdd078d09eca50687a77da1d40b
Darya Mazaleuskaya <dariayarohovich@gmail.com>
2022-03-20 13:44:58 +0100

    Edit production class names

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 import { MarketingApp } from "./components/marketing-app";
 import { Header } from "./components/header";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
+
 export const App = () => (
   <BrowserRouter>
-    <div>
-      <Header />
-      <MarketingApp />
-    </div>
+    <StylesProvider generateClassName={generateClassName}>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </StylesProvider>
   </BrowserRouter>
 );
